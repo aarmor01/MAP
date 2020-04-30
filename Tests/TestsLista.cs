d48272189eec48a8cd11068b2bd8b85177a83c01
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2020-04-30 19:45:09 +0200

    Creado dos tests para Lista

--- Tests/TestsLista.cs
+++ Tests/TestsLista.cs
@@ -2,19 +2,35 @@
 using NUnit.Framework;
 using Listas;
 
-
 namespace Tests
 {
     [TestFixture]
     public class TestsLista
     {
+        [SetUp]
+        public void InitVariables()
+        {
+
+        }
+
+        #region TestsCuentaElementos()
         [Test]
-        public void NumElemListaVacia()
+        public void NumElemListaVacia() //comprueba que l.CuentaElementos() funciona para una lista vacía
         {
             //Arrange
             Lista l = new Lista();
             //Act-Assert
             Assert.That(l.CuentaElementos(), Is.EqualTo(0), "Error: No se ha creado bien la lista");
         }
+
+        [Test]
+        public void NumElemListaNoVacia() //comprueba que l.CuentaElementos() funciona para una lista no vacía
+        {
+            //Arrange
+            Lista l = new Lista(4, 2);
+            //Act-Assert
+            Assert.That(l.CuentaElementos(), Is.EqualTo(8), "Error: No se ha creado bien la lista");
+        }
+        #endregion
     }
 }
