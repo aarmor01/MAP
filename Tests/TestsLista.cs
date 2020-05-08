d4183e18db1f7a6d059b2b41f31e4dfbb3d98279
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2020-05-08 20:18:33 +0200

    Añadido mas tests para Lista

--- Tests/TestsLista.cs
+++ Tests/TestsLista.cs
@@ -7,10 +7,14 @@ namespace Tests
     [TestFixture]
     public class TestsLista
     {
+        Lista listaVacia, listaNoVacia, listaConRepeticion;
+
         [SetUp]
         public void InitVariables()
         {
-
+            listaVacia = new Lista();
+            listaNoVacia = new Lista(3, 1);
+            listaConRepeticion = new Lista(4, 2);
         }
 
         #region Tests_CuentaElementos()
@@ -51,9 +55,9 @@ namespace Tests
         {
             //Arrange
             Lista l = new Lista(3, 2);
-            //Act
             int longitudPrevia = l.nElems;
             string listaPrevia = l.VerLista();
+            //Act
             l.Inserta(6);
             //Assert
             Assert.That(l.nElems, Is.EqualTo(longitudPrevia + 1), "Error: No se ha añadido el elemento a la lista (no ha aumentado el contador)");
@@ -73,7 +77,8 @@ namespace Tests
         }
 
         [Test]
-        public void BorraElementoLista1ElementoEsta() //comprueba que l.CuentaElementos() funciona para una lista no vacía
+        //comprueba que l.BorraElemento() funciona para una lista de 1 elemento cuando se borra el elemento en cuestion
+        public void BorraElementoLista1ElementoEsta()
         {
             //Arrange
             Lista l = new Lista(1, 1);
@@ -84,15 +89,75 @@ namespace Tests
         }
 
         [Test]
-        public void BorraElementoLista1ElementoNoEsta() //comprueba que l.CuentaElementos() funciona para una lista no vacía
+        //comprueba que l.BorraElemento() funciona para una lista de 1 elemento cuando se intenta borra un elemento que no está
+        public void BorraElementoLista1ElementoNoEsta()
         {
             //Arrange
             Lista l = new Lista(1, 1);
+            int nElemsPrevio = l.nElems;
+            string listaPrevia = l.VerLista();
             //Act-Assert
             Assert.That(l.BorraElemento(5), Is.False, "Error: Debería devolver 'false' porque el elemento no existe y no se ha eliminado");
-            //Assert.That(l.nElems, Is.EqualTo(0), "Error: No debería haber ningun elemento en la lista");
-            //Assert.That(l.VerLista(), Is.EqualTo(""), "Error: Hay algun elemento en la lista, y deberia estar vacía");
+            Assert.That(l.nElems, Is.EqualTo(nElemsPrevio), "Error: No debería haber ningun elemento en la lista");
+            Assert.That(l.VerLista(), Is.EqualTo(listaPrevia), "Error: Hay algun elemento en la lista, y deberia estar vacía");
         }
+
+        [Test]
+        //comprueba que l.BorraElemento() funciona para una lista de varios elemento cuando se intenta borra el primer elemento
+        public void BorraElementoPrimeroListaNoVacia()
+        {
+            //Arrange
+            Lista l = new Lista(7, 1);
+            int nElemsPrevio = l.nElems;
+            //Act-Assert
+            Assert.That(l.BorraElemento(1), Is.True, "Error: Debería devolver 'true' porque el elemento existe y se ha eliminado");
+            Assert.That(l.nElems, Is.EqualTo(nElemsPrevio - 1), "Error: Debería reducirse el numero de elementos en uno");
+            Assert.That(l.VerLista(), Is.EqualTo("2_3_4_5_6_7_"), "Error: La lista resultante de la eliminacion difiere con la prevista");
+        }
+        
+        [Test]
+        //comprueba que l.BorraElemento() funciona para una lista de varios elemento cuando se intenta borra el ultimo elemento
+        public void BorraElementoUltimoListaNoVacia()
+        {
+            //Arrange
+            Lista l = new Lista(7, 1);
+            int nElemsPrevio = l.nElems;
+            //Act-Assert
+            Assert.That(l.BorraElemento(7), Is.True, "Error: Debería devolver 'true' porque el elemento existe y se ha eliminado");
+            Assert.That(l.nElems, Is.EqualTo(nElemsPrevio - 1), "Error: Debería reducirse el numero de elementos en uno");
+            Assert.That(l.VerLista(), Is.EqualTo("1_2_3_4_5_6_"), "Error: La lista resultante de la eliminacion difiere con la prevista");
+        }
+        
+        [Test]
+        //comprueba que l.BorraElemento() funciona para una lista de varios elemento cuando se intenta borra un elemento medio
+        public void BorraElementoMedioListaNoVacia()
+        {
+            //Arrange
+            Lista l = new Lista(7, 1);
+            int nElemsPrevio = l.nElems;
+            //Act-Assert
+            Assert.That(l.BorraElemento(4), Is.True, "Error: Debería devolver 'true' porque el elemento existe y se ha eliminado");
+            Assert.That(l.nElems, Is.EqualTo(nElemsPrevio - 1), "Error: Debería reducirse el numero de elementos en uno");
+            Assert.That(l.VerLista(), Is.EqualTo("1_2_3_5_6_7_"), "Error: La lista resultante de la eliminacion difiere con la prevista");
+        }
+        
+        [Test]
+        //comprueba que l.BorraElemento() funciona para una lista de varios elemento cuando se intenta borra un elemento mediorepetido posteriormente
+        public void BorraElementoListaNoVaciaConRepeticion()
+        {
+            //Arrange
+            Lista l = new Lista(3, 2);
+            int nElemsPrevio = l.nElems;
+            //Act-Assert
+            Assert.That(l.BorraElemento(3), Is.True, "Error: Debería devolver 'true' porque el elemento existe y se ha eliminado");
+            Assert.That(l.nElems, Is.EqualTo(nElemsPrevio - 1), "Error: Debería reducirse el numero de elementos en uno");
+            Assert.That(l.VerLista(), Is.EqualTo("1_2_1_2_3_"), "Error: La lista resultante de la eliminacion difiere con la prevista");
+        }
+        #endregion
+
+        #region Tests_N_Esimo()
+
+
         #endregion
     }
 }
