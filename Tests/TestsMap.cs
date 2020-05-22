65171ba33643609e12f0e51c37cd0b135f36a370
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2020-05-22 17:12:53 +0200

    Primeros tests de Player y Map añadidos

--- Tests/TestsMap.cs
+++ Tests/TestsMap.cs
@@ -1,10 +1,58 @@
 ﻿using System;
 using NUnit.Framework;
 using Listas;
+using Adventure;
 
 namespace Tests
 {
-    class TestsMap
+    [TestFixture]
+    public class TestsMap
     {
+        Map map;
+
+        [SetUp]
+        public void CreateMap()
+        {
+            map = new Map(100, 100);
+            map.CreateMap(9, 5, new int[] { 3, 2, 3, 1, 4, 8}, 1, 8);
+        }
+
+        #region Tests_FindItemByName()
+        //comprueba que FindItemByName() funciona para un elemento medio
+        [Test]
+        public void FindItemByNameNombreMedio()
+        {
+            //Arrange en SetUp
+            //Act-Assert
+            Assert.That(map.FindItemByName("Item 2"), Is.EqualTo(2), "Error: el método no devuelve el indice del item para un elemento medio");
+        }
+
+        //comprueba que FindItemByName() funciona para el primer elemento
+        [Test]
+        public void FindItemByNamePrimerNombre()
+        {
+            //Arrange en SetUp
+            //Act-Assert
+            Assert.That(map.FindItemByName("Item 0"), Is.EqualTo(0), "Error: el método no devuelve el indice del item para el primer elemento");
+        }
+
+        //comprueba que FindItemByName() funciona para el ultimo elemento
+        [Test]
+        public void FindItemByNameUltimoNombre()
+        {
+            //Arrange en SetUp
+            //Act-Assert
+            Assert.That(map.FindItemByName("Item 4"), Is.EqualTo(4), "Error: el método no devuelve el indice del item para el ultimo elemento");
+        }
+        
+        //comprueba que FindItemByName() funciona cuando se introduce un elemento inexistente
+        [Test]
+        public void FindItemByNameNoExiste()
+        {
+            //Arrange en SetUp
+            //Act-Assert
+            Assert.That(map.FindItemByName("Item 5"), Is.EqualTo(-1), "Error: el método devuelve un indice pese a que no existe el item");
+        }
+        #endregion
     }
 }
--- Tests/TestsPlayer.cs
+++ Tests/TestsPlayer.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using NUnit.Framework;
 using Listas;
+using Adventure;
 
 namespace Tests
 {
-    class TestsPlayer
+    [TestFixture]
+    public class TestsPlayer
     {
+        #region Test_ Player()
+        [Test]
+        public void Constructora()
+        {
+            //Arrange
+            Player player = new Player("Gato de Guillermo", 1);
+
+            //Act-Assert
+            Assert.That(player.GetPlayerInfo(), Is.EqualTo("Name: Gato de Guillermo HP: 10 Inventory weight: 0"),
+                "Error: El nombre del jugador, HP del mismo o peso de su inventario no están bien establecido/s");
+            Assert.That(player.GetPosition(), Is.EqualTo(1), "Error: sala inicial establecida erroneamente");
+            Assert.That(player.inventory.VerLista(), Is.EqualTo(""), "Error: inventario creado erroneamente");
+        }
+        #endregion
     }
 }
